ef168d3d0f6b20ab8932bf61660d744d23e1d6d8
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-03-10 02:12:46 -0600

    Deploy latest version

---
+++ js/env.js
@@ -0,0 +1,4 @@
+window.env = {
+  //apiUrl: 'http://localhost:3000/api/v1'
+  apiUrl: 'http://stage.lifeapp.bitspacedevelopment.com/api/v1'
+};
--- js/ionic.app.js
+++ js/ionic.app.js
@@ -1,26 +1,10 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
+// LiFE App!
 
 window.lifeApp = {
   application: angular.module('lifeApp', ['ionic', 'lifeApp.lib', 'lifeApp.controllers', 'lifeApp.services', 'ngResource', 'ng-token-auth', 'rails', 'ngRoute', 'ngSanitize']),
   controllers: angular.module('lifeApp.controllers', []),
   services: angular.module('lifeApp.services', []),
   lib: angular.module('lifeApp.lib', [])
-};
-
-window.env = {
-
-  /***Development***/
-  //apiUrl: 'http://localhost:3000/api/v1'
-
-  /***Production***/
-  apiUrl: 'http://stage.lifeapp.bitspacedevelopment.com/api/v1'
-
 };
 
 lifeApp.application.config(function($httpProvider, $authProvider, $stateProvider, $urlRouterProvider) {
@@ -58,20 +42,23 @@ lifeApp.application.config(function($httpProvider, $authProvider, $stateProvider
   })
   .state('tabs.intro-survey', {
     url: 'intro-survey',
-    views: {
-      'home-tab': {
-        templateUrl: 'templates/intro-survey.html',
-        controller: 'IntroSurveyCtrl'
-      }
-    },
     resolve: {
       auth: function($auth) {
         return $auth.validateUser();
+      },
+      introQuestions: function(IntroQuestion) {
+        return IntroQuestion.query();
+      }
+    },
+    views: {
+      'home-tab': {
+        templateUrl: 'templates/intro-question.html',
+        controller: 'IntroQuestionCtrl'
       }
     }
   })
   .state('tabs.survey', {
-    url: 'survey/:id',
+    url: 'survey/{id:int}',
     views: {
       'home-tab': {
         templateUrl: 'templates/survey.html',
@@ -155,93 +142,107 @@ lifeApp.application.run(function($ionicPlatform) {
   });
 });
 
+var indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+
+lifeApp.lib.constant('Module', function() {
+  var Module;
+  return Module = (function() {
+    function Module() {}
+
+    Module.KEYOWRDS = ['extended', 'included'];
+
+    Module.extend = function(obj) {
+      var key, ref, value;
+      for (key in obj) {
+        value = obj[key];
+        if (indexOf.call(Module.KEYOWRDS, key) < 0) {
+          this[key] = value;
+        }
+      }
+      if ((ref = obj.extended) != null) {
+        ref.apply(this);
+      }
+      return this;
+    };
+
+    Module.include = function(obj) {
+      var key, ref, value;
+      for (key in obj) {
+        value = obj[key];
+        if (indexOf.call(Module.KEYOWRDS, key) < 0) {
+          this.prototype[key] = value;
+        }
+      }
+      if ((ref = obj.included) != null) {
+        ref.apply(this);
+      }
+      return this;
+    };
+
+    return Module;
+
+  })();
+});
+
 lifeApp.controllers.controller('CategoryIndexCtrl', function($scope, Category) {
   Category.query().$promise.then(function(response){
     $scope.categorys = response;
   });
 });
 
-lifeApp.controllers.controller('HomePropertiesCtrl', function($scope, $http){
-  $http.get(env.apiUrl + '/property/home').then(function(resp) {
-    $scope.home = resp.data;
-  });
+lifeApp.controllers.controller('HomePropertiesCtrl',
+  function($scope, Property) {
+
+    var homeProps   = ['home', 'home_logged_in', 'home_logged_out'];
+
+    homeProps.forEach(function(name) {
+      Property.get({ name: name }, function(prop) {
+        $scope[name] = prop;
+      });
+    });
+  }
+);
+
+lifeApp.controllers.controller('IntroQuestionCtrl', function($scope, $state, introQuestions, IntroQuestion, IntroResponse, User){
+
+	$scope.intro_questions = introQuestions;
+	$scope.questionIndex   = 0;
+
+	$scope.respondWith = function(introAnswer) {
+
+		IntroResponse.create({ intro_answer_id: introAnswer.id }, {}, function(introResponse) {
+
+			$scope.questionIndex += 1;
+
+			if ($scope.questionIndex >= $scope.intro_questions.length) {
+				$scope.introCompleted();
+			}
+		});
+	};
+
+	$scope.introCompleted = function() {
+
+		IntroQuestion.complete(function(complete) {
+
+			User.get(function(user) {
+
+				$scope.user = user; // reload user and go to survey index
+				$state.go('tabs.survey-index');
+			});
+		});
+	}
 });
 
-lifeApp.controllers.controller('IntroSurveyCtrl', function($scope, $state, $auth, IntroSurvey, User, Response){
-	// set up variables
-	var index = 0;
-	var userID = 0;
-	
-	$scope.user = $auth.user;
-	userID = $auth.user.id;
-	var user = User.get({id: userID});
-	//load intro survey
-	IntroSurvey.query().$promise.then(function(results){
-		$scope.questions = results;
-		if(user.question_id != null){
-			for(var i = 0; i < results.length - 1; i ++){
-				if(user.question_id == results[i].id){
-					index = i;
-				}
-			}
-			showIntroSurvey(index);
-		}else{
-			index = 0;
-			showIntroSurvey(index);
-		}
-		
-		function showIntroSurvey(index){
-			//scope binding question and answer data by question id
-			$scope.question = results[index];
-			$scope.answers = results[index].intro_answers;
-
-			//set init ng-model answers
-			$scope.data = {answers: ''};
-
-			//when user click answer
-			$scope.next = function(answer){
-				//create response
-				var response = new Response();
-				response.answer_id = answer.id;
-				response.user_id = userID;
-				response.$save();
-				//update user info
-				user.question_id = results[index].id;
-				user.intro_quiz = false;
-				User.update({id: userID}, user);
-				//increments
-				index ++;
-				//reset socre
-				score = 0;
-				//if going to last question
-				if (index > results.length - 1){		
-					user.intro_quiz = true;
-					User.update({id: userID}, user);
-					//redriesct page to inex survey page
-					$state.go('tabs.survey-index');
-					//reset index number
-					index = 0;
-				}
-				//next question
-				else{
-					$scope.question = results[index];
-					$scope.answers = results[index].intro_answers;
-					$scope.data = {answers: ''};
-				}
-			}
-		}
-	});
-});
-lifeApp.controllers.controller('LandingPropertiesCtrl', function($scope, $http){
-  $http.get(env.apiUrl + '/property/about').then(function(resp) {
-    $scope.about = resp.data;
+lifeApp.controllers.controller('LandingPropertiesCtrl', function($scope, Property){
+  Property.get({ name: 'about' }, function(prop) {
+    $scope.about = prop;
   });
 });
 
 lifeApp.controllers.controller('ResourceCtrl', function($scope, $stateParams, $ionicPopup, $state, Resource, AllResources, $http, $auth) {
 	var id = $stateParams.categoryId;
 
-  var resourceProvider = !id ? AllResources.query() : Resource.query({categoryId: id});
+  var resourceProvider = !id ? AllResources.query() : Resource.query({category_id: id});
 
     //get resources
     resourceProvider.$promise.then(function(response){
@@ -263,35 +264,39 @@ lifeApp.controllers.controller('ResourceCtrl', function($scope, $stateParams, $i
     });
 
     //add favourite resource
-    $scope.favouritedResource = function favouritedResource(id) {
-        url = env.apiUrl + "/favourite/" + id;
+    $scope.favouritedResource = function favouritedResource(resource) {
 
-        $http.get(url).success( function(response) {
-            if(response.length > 1) {
-                errormsg = response[0];
-                var show = $ionicPopup.confirm({
-                    title: errormsg,
-                    buttons: [{
-                        text: 'Cancel',
-                        type: 'button-default',
-                      }, {
-                        text: 'Log In',
-                        type: 'button-positive',
-                        onTap: function(e) {
-                            $state.go('tabs.signin');
-                        }
-                      }]
-                });
-            }else {
-                if(response == 'true'){
-                    document.getElementById(id).className = "button button-assertive";
-                    document.getElementById(id+'icon').className = "icon ion-ios-heart";
-                } else {
-                    document.getElementById(id).className = "button button-energized";
-                    document.getElementById(id+'icon').className = "icon ion-ios-star";
-                }
-            }
-        });
+				var url = env.apiUrl;
+				if (resource.favourite) {
+					 url += '/resources/' + id + '/unfavourite';
+				} else {
+					url += '/resources/' + id + '/favourite';
+				}
+
+        $http.put(url)
+					// resource updated successfully
+					.then(function(response) {
+						resource.favourite = response.data.favourite;
+					})
+					// an error happened, probably a 401
+					.catch(function(error) {
+						if (error.status != 401) { return; }
+
+						var errorMsg = error.data.errors[0],
+						        show = $ionicPopup.confirm({
+							title: errorMsg,
+							buttons: [
+								{
+									text: 'Cancel',
+									type: 'button-default',
+								}, {
+									text: 'Log In',
+									type: 'button-positive',
+									onTap: function(e) { $state.go('tabs.signin'); }
+								}
+							]
+						});
+        	});
     }
 });
 
@@ -315,109 +320,129 @@ lifeApp.controllers.controller('SignUpCtrl', function($scope, $state, $ionicPopu
   };
 });
 
-lifeApp.controllers.controller('SurveyCtrl', function($scope, $stateParams, $auth, $http, SecondTierQuestion, RecommendResources, Survey, SurveyQuestion) {
+lifeApp.controllers.controller('SurveyCtrl', function($scope, $stateParams, $auth, $http, SecondTierQuestion, RecommendResources, Survey, SurveyQuestion, IntroResponse) {
 	var category_id = $stateParams.id;
 
-    var score = 0;
-    var index = 0;
-    var totalScore = 0;
-
-    var resources = [];
-    var scores = [];
+  var score      = 0,
+			index      = 0,
+			totalScore = 0,
+			resources  = [],
+			scores     = [];
 
 	$scope.user = $auth.user;
 	var user_id = $auth.user.id;
-	//evaluate response
-	$http.get(env.apiUrl + '/responses/' + user_id).then(function(resp){
-		buildQuestion(index);
 
-		function buildQuestion(index){
-			SecondTierQuestion.query({introAnswerId: resp.data[index].answer_id, categoryId: category_id}).$promise.then(function(results){
-				if(results.length != 0){
+	// evaluate response
+	IntroResponse.query().$promise.then(function(responses) {
+
+		if (responses.length) { buildQuestion(index); }
+
+		function buildQuestion(index) {
+
+			SecondTierQuestion.query({ intro_answer_id: responses[index].answer_id, category_id: category_id }).$promise.then(function(results) {
+
+				if (results.length != 0) {
 					$scope.end = false;
-			    	$scope.showResources = false;
+			    $scope.showResources = false;
 
 					$scope.questions = results;
 					var question_index = 0;
 					$scope.data = {answers: ''};
 					$scope.question = results[question_index];
 					$scope.answers = results[question_index].answers;
-					
 
-					$scope.next = function(answer){
+
+					$scope.next = function(answer) {
 						score = answer.score;
 						scores.push(score);
-						//set recommend resources
-						if(score > 0){
-		        			//need resources
-		        			resources.push(RecommendResources.query({questionId: results[question_index].id}));
-		        		}
-		        		question_index ++;
-						if(question_index >results.length - 1){
+
+						// set recommend resources
+						if (score > 0) {
+		        	// need resources
+		        	resources.push(RecommendResources.query({question_id: results[question_index].id}));
+		      	}
+
+		        question_index ++;
+
+						if (question_index >results.length - 1) {
 							checkNext(index);
-						}else{
+						} else {
 							$scope.data = {answers: ''};
 							$scope.question = results[question_index];
 							$scope.answers = results[question_index].answers;
-						}	
+						}
 					}
-				}else{
+				} else {
 					checkNext(index);
 				}
 			});
 		}
 
-		function checkNext(index){
-			index ++;
-			if(index > resp.data.length -1){
-				//universal condition question
-				Survey.query({id: category_id}).$promise.then(function(survey){
-					if(survey.length != 0){
-						var s_id = survey[0].id;
-						SurveyQuestion.query({id: s_id}).$promise.then(function(questions){
-						if(questions.length !=0){
-							$scope.questions = questions
-							var q_index = 0;
-							$scope.data = {answers: ''};
-							$scope.question = questions[q_index];
-							$scope.answers = questions[q_index].answers;
+		function checkNext(index) {
 
-							$scope.next =function(answer){
-								score = answer.score;
-								scores.push(score);
-								//set recommend resources
-								if(score > 0){
-				        			//need resources
-				        			resources.push(RecommendResources.query({questionId: questions[q_index].id}));
-				        		}
-				        		q_index ++;
-								if(q_index > questions.length - 1){
-									showResources()
-								}else{
-									$scope.data = {answers: ''};
-									$scope.question = questions[q_index];
-									$scope.answers = questions[q_index].answers;
-								}	
+			index ++;
+
+			if (index > responses.length -1) {
+
+				// universal condition question
+				Survey.query({ category_id: category_id }).$promise.then(function(survey) {
+
+					if (survey.length != 0) {
+
+						var s_id = survey[0].id;
+
+						SurveyQuestion.query({ survey_id: s_id }).$promise.then(function(questions) {
+
+							if (questions.length !=0) {
+
+								$scope.questions = questions
+								var q_index = 0;
+								$scope.data = {answers: ''};
+								$scope.question = questions[q_index];
+								$scope.answers = questions[q_index].answers;
+
+								$scope.next =function(answer) {
+
+									score = answer.score;
+									scores.push(score);
+
+									//set recommend resources
+									if (score > 0) {
+					        	//need resources
+					        	resources.push(RecommendResources.query({ question_id: questions[q_index].id}));
+					        }
+
+					        q_index ++;
+
+									if (q_index > questions.length - 1) {
+										showResources()
+									} else {
+										$scope.data = {answers: ''};
+										$scope.question = questions[q_index];
+										$scope.answers = questions[q_index].answers;
+									}
+								}
+							} else {
+								showResources();
 							}
-						}else{
-							showResources();
-						}
 						});
-					}else{
+					} else {
 						showResources();
 					}
-				});	
-			}else{
+				});
+			} else {
 				buildQuestion(index);
 			}
 		}
 
-		function showResources(){
+		function showResources() {
 			$scope.end = true;
-			for(var i = 0; i < scores.length - 1; i ++){
+
+			for (var i = 0; i < scores.length - 1; i ++) {
 				totalScore += scores[i];
 			}
-			if(totalScore > 0){
+
+			if (totalScore > 0) {
 				$scope.showResources = true;
 				$scope.showResources = resources;
 			}
@@ -425,9 +450,9 @@ lifeApp.controllers.controller('SurveyCtrl', function($scope, $stateParams, $aut
 	});
 });
 
-lifeApp.controllers.controller('SurveyEndPropertiesCtrl', function($scope, $http){
-  $http.get(env.apiUrl + '/property/surveyend').then(function(resp) {
-    $scope.surveyend = resp.data;
+lifeApp.controllers.controller('SurveyEndPropertiesCtrl', function($scope, Property){
+  Property.get({ name: 'survey_end' }, function(prop) {
+    $scope.surveyend = prop;
   });
 });
 
@@ -492,51 +517,9 @@ lifeApp.controllers.controller('UserSessionCtrl', function($scope, $state, $wind
   };
 });
 
-var indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
-
-lifeApp.lib.constant('Module', function() {
-  var Module;
-  return Module = (function() {
-    function Module() {}
-
-    Module.KEYOWRDS = ['extended', 'included'];
-
-    Module.extend = function(obj) {
-      var key, ref, value;
-      for (key in obj) {
-        value = obj[key];
-        if (indexOf.call(Module.KEYOWRDS, key) < 0) {
-          this[key] = value;
-        }
-      }
-      if ((ref = obj.extended) != null) {
-        ref.apply(this);
-      }
-      return this;
-    };
-
-    Module.include = function(obj) {
-      var key, ref, value;
-      for (key in obj) {
-        value = obj[key];
-        if (indexOf.call(Module.KEYOWRDS, key) < 0) {
-          this.prototype[key] = value;
-        }
-      }
-      if ((ref = obj.included) != null) {
-        ref.apply(this);
-      }
-      return this;
-    };
-
-    return Module;
-
-  })();
-});
-
 lifeApp.application.config(function($authProvider) {
   $authProvider.configure({
-    apiUrl: env.apiUrl,
+    apiUrl:   env.apiUrl,
     storage: 'localStorage'
   });
 });
@@ -549,42 +532,60 @@ lifeApp.services.factory('Category', function($resource) {
   return $resource(env.apiUrl + "/categories/:id");
 });
 
-lifeApp.services.factory('User', function($resource){
-	return $resource(env.apiUrl + '/user/:id', {id: '@id'}, 
-		{
-			'update': {method: 'PUT'}
-		});
+lifeApp.services.factory('IntroQuestion', function($resource){
+	return $resource(
+			env.apiUrl + '/intro_questions',
+			null,
+			// NOTE: There must be a better way than this... relative url path?
+			{ complete: { method: 'PUT', url: env.apiUrl + '/intro_questions/complete' } }
+	);
 });
-lifeApp.services.factory('IntroSurvey', function($resource){
-	return $resource(env.apiUrl + '/introQuestions');
+
+lifeApp.services.factory('IntroResponse', function($resource) {
+  return $resource(
+    env.apiUrl + '/intro_responses',
+    null,
+    { create: { method: 'POST', url: env.apiUrl + '/intro_answers/:intro_answer_id/intro_responses' } }
+  );
 });
+
+lifeApp.services.factory('Property', function($resource){
+	return $resource(env.apiUrl + '/properties/:name');
+});
+
 lifeApp.services.factory('Question', function($resource){
 	return $resource(env.apiUrl + '/questions/:id', {id: '@id'});
 });
 
 lifeApp.services.factory('RecommendResources', function($resource){
-	return $resource(env.apiUrl + '/recommendResources/:questionId', {questionId: '@id'});
+	return $resource(env.apiUrl + '/questions/:question_id/resources', { question_id: '@id'});
 });
 
 lifeApp.services.factory('Resource', function($resource) {
-  return $resource(env.apiUrl + "/categories/:categoryId/resources", {categoryId: '@id'});
+  return $resource(
+      env.apiUrl + "/categories/:category_id/resources",
+      { category_id: '@id' }
+  );
 });
-
 
 lifeApp.services.factory('Response', function($resource) {
-  return $resource(env.apiUrl + "/response");
-});
-lifeApp.services.factory('SecondTierQuestion', function($resource){
-	return $resource(env.apiUrl + '/secondTierQuestions/:introAnswerId/:categoryId', {introAnswerId: '@intro_answer_id', categoryId: '@category_id'});
+  return $resource(env.apiUrl + '/responses');
 });
 
+lifeApp.services.factory('SecondTierQuestion', function($resource){
+	return $resource(env.apiUrl + '/categories/:category_id/questions', { category_id: '@category_id', intro_answer_id: '@intro_answer_id' });
+});
 
 lifeApp.services.factory('Survey', function($resource){
-	return $resource(env.apiUrl + '/universalSurveys/:id', {id: '@id'});
+	return $resource(env.apiUrl + '/categories/:category_id/surveys', { category_id: '@category_id'});
 });
 
 lifeApp.services.factory('SurveyQuestion', function($resource){
-	return $resource(env.apiUrl + '/surveyQuestions/:id');
+	return $resource(env.apiUrl + '/surveys/:survey_id/questions', { survey_id: '@survey_id' });
+});
+
+lifeApp.services.factory('User', function($resource){
+	return $resource(env.apiUrl + '/auth/user');
 });
 
 lifeApp.services.factory('UserSession', function($resource) {
